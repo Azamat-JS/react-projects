2e4c6a88b04c3e94e519821fdbc8a26a2e3bf131
Azamat-JS <azamatielts987@gmail.com>
2025-01-14 21:34:51 +0500

    react components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header";
+import Entry from "../components/Entry";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="container">
+      <Entry />
+      <Entry />
+      </main>
+    </>
+  );
+}
+
+export default App;
