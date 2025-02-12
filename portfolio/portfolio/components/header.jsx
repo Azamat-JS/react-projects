773b25a7ac1e0d46f65b5dad681a3747dd1ad535
Azamat-JS <azamatielts987@gmail.com>
2025-02-12 05:56:49 +0500

    started portfolio project

---
+++ portfolio/portfolio/components/header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return (
+        <header>
+        <img src="../images/nodelogo.png" alt="nodejsLogo" />
+        <p>this is my portfolio</p>
+        </header>
+    )
+}
