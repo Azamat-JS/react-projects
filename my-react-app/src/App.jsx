14b365be215f67b55d027c110be5bac996f7bed8
Azamat-JS <azamatielts987@gmail.com>
2025-06-02 21:47:41 +0500

    ternary operators in React

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,18 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import Card from './components/Card';
-import Button from './components/Button';
-import img1 from './assets/hero.png'
-import img2 from './assets/hero_img.jpg'
+
+import UserGreeting from './components/UserGreeting';
 
 
 const App = () => {
 
   return (
     <main>
-      <Card img={img1} title='Azamat Developer' text="Node.js web developer for more than 6 months"/>
-      <Card img={img2} title='Davron Designer' text="Figma designer for more than 6 months"/>
-      <Card img={img1} title='Odil SMM' text="SMM manager in X company for more than 10 months"/>
-      <Button/>
+      <UserGreeting isLoggedIn={true} username="Azamat Abdulloh"/>
     </main>
   )
 }
--- my-react-app/src/components/Card.jsx
+++ my-react-app/src/components/Card.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import ImgE from   '../assets/hero.png'
+import ImgE from   '../assets/hero.png';
+import PropTypes from 'prop-types';
 
-const Card = ({img, title, text}) => {
+const Card = (props) => {
   return (
     <div className='card'>
-        <img className='card-img' src={img} alt="Picture for web" />
-        <h2 className='card-title'>{title}</h2>
-        <p className='card-text'>{text}</p>
+        <img className='card-img' src={props.img} alt="Picture for web" />
+        <h2 className='card-title'>{props.title}</h2>
+        <p className='card-text'>{props.text}</p>
+        <p>{props.rating}</p>
     </div>
   )
+}
+
+Card.propTypes = {
+  img: PropTypes.string,
+  title: PropTypes.string,
+  text: PropTypes.string,
+  rating: PropTypes.number,
+}
+
+Card.defaultProps = {
+  img: ImgE,
+  title: "Green Card",
+  text: "Card has some text",
+  rating: 4.5
 }
 
 export default Card
