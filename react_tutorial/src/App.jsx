5735a6f97ebb57d2f0e607f48433de0808ad9830
Azamat-JS <azamatielts987@gmail.com>
2025-06-28 23:13:56 +0500

    image slider project completed

--- react_tutorial/src/App.jsx
+++ react_tutorial/src/App.jsx
@@ -3,20 +3,24 @@ import Accordian from './components/accordian/Accordian'
 import './App.css'
 import RandomColor from './components/random-color'
 import StarRating from './components/star-rating'
+import ImageSlider from './components/image-slider'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    {/* Accordian component */}
-     {/* <Accordian /> */}
+      {/* Accordian component */}
+      {/* <Accordian /> */}
 
-     {/* Random color component */}
-     {/* <RandomColor/> */}
+      {/* Random color component */}
+      {/* <RandomColor/> */}
 
-     {/* Star rating component */}
-    <StarRating/>
+      {/* Star rating component */}
+      {/* <StarRating/> */}
+
+      {/* Image slider component */}
+      <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'} />
     </>
   )
 }
