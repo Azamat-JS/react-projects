c1cee366e7fbbb190085358462f5d6c7570c8a82
Azamat-JS <azamatielts987@gmail.com>
2025-06-28 14:38:16 +0500

    New React projects started

---
+++ react_tutorial/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Accordian from './components/accordian/Accordian'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Accordian />
+    </>
+  )
+}
+
+export default App
