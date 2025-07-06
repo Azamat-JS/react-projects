bed8f9af81eca20d1fa5fd43df22782d8d8f0560
Azamat-JS <azamatielts987@gmail.com>
2025-07-06 19:43:13 +0500

    completed autocomplete-search project

--- projects-react/src/components/search-autocomplete/Autocomplete.jsx
+++ projects-react/src/components/search-autocomplete/Autocomplete.jsx
@@ -1,46 +1,80 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import Suggestions from "./Suggestions";
 
 const Autocomplete = () => {
-    const [loading, setLoading] = useState(false);
-    const [users, setUsers] = useState([]);
-    const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(null);
+  const [searchParam, setSearchParam] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [filteredUsers, setFilteredUsers] = useState([]);
 
-    async function fetchListOfUsers(){
-        try {
-            setLoading(true)
-            const response = await fetch('https://dummyjson.com/users');
-            const data = await response.json();
-
-            console.log(data)
-            if(data && data.users && data.users.length){
-                setUsers(data.users.map(userItem => userItem.firstName))
-                setLoading(false);
-                setError(null)
-            }
-        } catch (error) {
-            setLoading(false)
-            console.error(error)
-            setError(error)
-        }
+  function handleChange(event) {
+    const query = event.target.value.toLowerCase();
+    setSearchParam(query);
+    if (query.length > 1) {
+      const filteredData =
+        users && users.length
+          ? users.filter((item) => item.toLowerCase().indexOf(query) > -1)
+          : [];
+      setFilteredUsers(filteredData);
+      setShowDropdown(true);
+    } else {
+      setShowDropdown(false);
     }
+  };
 
-    useEffect(() => {
-        fetchListOfUsers()
-    }, []);
+  function handleClick(event){
+    console.log(event.target.innerText)
+    setShowDropdown(false)
+    setSearchParam(event.target.innerText)
+    setFilteredUsers([])
+  }
 
-    console.log(users)
+  async function fetchListOfUsers() {
+    try {
+      setLoading(true);
+      const response = await fetch("https://dummyjson.com/users");
+      const data = await response.json();
 
-    if(loading){
-        return <h1>Waiting for fetching users...</h1>
+      console.log(data);
+      if (data && data.users && data.users.length) {
+        setUsers(data.users.map((userItem) => userItem.firstName));
+        setLoading(false);
+        setError(null);
+      }
+    } catch (error) {
+      setLoading(false);
+      console.error(error);
+      setError(error);
     }
+  }
+
+  useEffect(() => {
+    fetchListOfUsers();
+  }, []);
+
+  console.log(users, filteredUsers);
+
+  if (loading) {
+    return <h1>Waiting for fetching users...</h1>;
+  }
   return (
-    <div className='search-autocomplete-container'>
-        <input 
-        type="text"
-        name='search-users' 
-        placeholder='Search Users here...'/>
+    <div className="search-autocomplete-container">
+      {loading ? (
+        <h1>Loading data...</h1>
+      ) : (
+        <input
+          value={searchParam}
+          type="text"
+          name="search-users"
+          placeholder="Search Users here..."
+          onChange={handleChange}
+        />
+      )}
+      {showDropdown && <Suggestions data={filteredUsers} handleClick={handleClick}/>}
     </div>
-  )
-}
+  );
+};
 
-export default Autocomplete
+export default Autocomplete;
---
+++ projects-react/src/components/search-autocomplete/Suggestions.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Suggestions = ({data, handleClick}) => {
+  return (
+    <ol>
+        {
+            data && data.length ? 
+            data.map((item, index) => <li onClick={handleClick} key={index}>{item}</li>)
+            : null
+        }
+    </ol>
+  )
+}
+
+export default Suggestions
