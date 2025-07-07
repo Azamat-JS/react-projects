abbd3e7a09165c7b233f26233391d39840d9e1f6
Azamat-JS <azamatielts987@gmail.com>
2025-07-07 19:30:09 +0500

    completed custom hook to show and hide content

--- projects-react/src/components/use-outside-click/TestClick.jsx
+++ projects-react/src/components/use-outside-click/TestClick.jsx
@@ -1,23 +1,29 @@
-
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react';
+import useClick from './UseClick';
 
 const TestClick = () => {
-    const [showContent, setShowContent] = useState(false)
+  const [showContent, setShowContent] = useState(false);
+  const ref = useRef();
 
-    function handleClose(){
-        setShowContent(false)
-    }
+  useClick(ref, () => setShowContent(false));
+
+  function handleClose() {
+    setShowContent(false);
+  }
+
   return (
-    <div className='close-container'>
-        {
-            showContent ? <div>
-                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quidem ratione non voluptatibus tempora, commodi quae blanditiis minus, atque tempore, nostrum quaerat? Iusto repudiandae suscipit vel magnam quasi libero cumque illum labore cum, mollitia obcaecati, odio asperiores amet, esse perferendis!</h1>
-                <p>Please click outside of this to close this. It won't close if you click inside of this content</p>
-                <button onClick={handleClose}>&times;</button>
-            </div> : <button onClick={() => setShowContent(true)}>Show Content</button>
-        }
+    <div ref={ref}>
+      {showContent ? (
+        <div>
+          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit...</h1>
+          <p>Please click outside of this to close this.</p>
+          <button onClick={handleClose}>&times;</button>
+        </div>
+      ) : (
+        <button onClick={() => setShowContent(true)}>Show Content</button>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default TestClick
+export default TestClick;
--- projects-react/src/components/use-outside-click/UseClick.jsx
+++ projects-react/src/components/use-outside-click/UseClick.jsx
@@ -1,27 +1,22 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react';
 
-const UseClick = (ref, handler) => {
+const useClick = (ref, handler) => {
+  useEffect(() => {
+    function listener(event) {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      handler(event);
+    }
 
-    useEffect(() => {
-        function listener(event){
-            if(!ref.current || ref.current.contains(event.target)) return;
-            handler(event)
-        };
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
 
-        document.addEventListener('mousedown', listener)
-        document.addEventListener('touchstart', listener)
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]);
+};
 
-        return () => {
-            document.removeEventListener('mousedown', listener)
-            document.removeEventListener('touchstart', listener)
-        }
-    }, [handler, ref])
-  return (
-    <div>
-
-        
-    </div>
-  )
-}
-
-export default UseClick
+export default useClick;
