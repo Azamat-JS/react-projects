e540e3fc55f9ac34b47ad86f37738e4ed03ae3b9
Azamat-JS <azamatielts987@gmail.com>
2025-07-09 18:02:40 +0500

    redux toolkit implemented in Shopping Cart project

--- shopping-cart/src/components/Products.jsx
+++ shopping-cart/src/components/Products.jsx
@@ -1,30 +1,66 @@
-import React, { useState } from 'react'
-import {FaHeart} from 'react-icons/fa'
+import { useState } from "react";
+import { FaHeart } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, removeFromCart } from "../store/slices/cart-slice";
 
-const Products = ({product}) => {
-    const [liked, setLiked] = useState(false);
+const Products = ({ product }) => {
+  const [liked, setLiked] = useState(false);
+  const dispatch = useDispatch();
+  const { cart } = useSelector((state) => state);
 
-    const toggleLike = () => setLiked(!liked)
+  function handleAddToCart() {
+    dispatch(addToCart(product));
+  }
+
+  function handleRemoveFromCart() {
+    console.log("removed");
+    dispatch(removeFromCart(product.id));
+  }
+
+  const toggleLike = () => setLiked(!liked);
   return (
-    <div className='relative'>
-            <FaHeart
-              onClick={toggleLike}
-              className={`w-5 h-5 absolute right-4 top-14 ${
-                liked ? 'text-red-500' : 'text-black'}`}/>
-        <div className='group flex flex-col items-center border-2 border-red-900 gap-3 p-4 h-[360px] mt-10 ml-5 rounded-xl'>
-            <div className='h-[180px]'>
-                <img src={product?.image} alt={product.title}
-                    className='object-cover w-[80%] h-full' />
-            </div>
-            <div>
-                <h1 className='w-40 truncate mt-3 text-gray-700 font-bold text-lg'>{product?.title}</h1>
-            </div>
-            <div className='flex items-center justify-center w-full mt-5'>
-                <button className='bg-red-950 text-white border-2 rounded-xl font-bold p-4'>Add to Cart</button>
-            </div>
+    <div className="relative">
+      <FaHeart
+        onClick={toggleLike}
+        className={`w-5 h-5 absolute right-4 top-14 ${
+          liked ? "text-red-500" : "text-black"
+        }`}
+      />
+      <div className="group flex flex-col items-center border-2 border-red-900 gap-3 p-4 mt-10 ml-5 rounded-xl">
+        <div className="h-[180px]">
+          <img
+            src={product?.image}
+            alt={product.title}
+            className="object-cover w-[80%] h-full"
+          />
         </div>
+        <div>
+          <h1 className="w-40 truncate mt-3 text-gray-700 font-bold text-lg">
+            {product?.title}
+          </h1>
+        </div>
+        <div>
+          <p className="text-gray-700 font-semibold text-medium">
+            ${product?.price}
+          </p>
+        </div>
+        <div className="flex items-center justify-center w-full mt-5">
+          <button
+            onClick={
+              cart.some((item) => item.id === product.id)
+                ? handleRemoveFromCart
+                : handleAddToCart
+            }
+            className="bg-red-950 text-white border-2 hover:bg-red-700 active:bg-red-400 rounded-xl font-bold p-4"
+          >
+            {cart.some((item) => item.id === product.id)
+              ? "Remove from Cart"
+              : "Add to Cart"}
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
--- shopping-cart/src/pages/Cart.jsx
+++ shopping-cart/src/pages/Cart.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const [totalCard, setTotalCard] = useState(0);
+  const { cart } = useSelector((state) => state);
 
-export default Cart
+  useEffect(() => {
+    setTotalCard(cart.reduce((acc, curr) => acc + curr.price, 0));
+  }, [cart]);
+  console.log(cart, totalCard);
+  return (
+    <div>
+      {cart && cart.length ? (
+        <div className="min-h-[80vh] grid md:grid-cols-2 max-w-6xl mx-auto"></div>
+      ) : (
+        <div className="min-h-[80vh] flex flex-col items-center justify-center">
+          <h1 className="text-gray-800 font-bold text-xl mb-2">
+            Your cart is empty
+          </h1>
+          <Link to={"/"}>
+            <button className="bg-red-950 text-white border-2 hover:bg-red-700 active:bg-red-400 rounded-xl font-bold p-4">
+              SHOP NOW
+            </button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
