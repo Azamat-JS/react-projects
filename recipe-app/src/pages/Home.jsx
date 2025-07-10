acbd972010f3429ffa814064a3a66550417abdf9
Azamat-JS <azamatielts987@gmail.com>
2025-07-10 19:13:34 +0500

    Added footer to Food Recipe app

--- recipe-app/src/pages/Home.jsx
+++ recipe-app/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, {useContext} from 'react'
 import { GlobalContext } from '../components/Context'
 import RecipeItem from '../components/RecipeList'
+import Footer from '../components/Footer';
 
 const Home = () => {
   const {recipeList, loading} = useContext(GlobalContext);
@@ -9,15 +10,18 @@ const Home = () => {
     return <h2>Loading... Please wait!</h2>
   }
   return (
+    <>
     <div className='py-8 container mx-auto flex flex-wrap justify-center gap-10'>
       {
         recipeList && recipeList.length > 0 ?
           recipeList.map((item) => <RecipeItem key={item.idMeal} item={item}/>)
-        : <div>
+          : <div>
           <p className='lg:text-4xl text-xl text-center text-black font-extrabold'>To see the meals please search</p>
         </div>
       }
     </div>
+    <Footer />
+      </>
   )
 }
 
