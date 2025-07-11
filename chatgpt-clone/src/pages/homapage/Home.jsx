98be91d920aec150b0930cf88aa0133f97736ae0
Azamat-JS <azamatielts987@gmail.com>
2025-07-12 00:03:32 +0500

    Completed Home page of ZET AI project

--- chatgpt-clone/src/pages/homapage/Home.jsx
+++ chatgpt-clone/src/pages/homapage/Home.jsx
@@ -1,49 +1,83 @@
-import './Home.css'
-import { Link } from 'react-router-dom'
-import { TypeAnimation } from 'react-type-animation'
+import { useState } from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import { TypeAnimation } from "react-type-animation";
 
 const Home = () => {
+  const [typingStatus, setTypingStatus] = useState("AI-bot");
   return (
-    <div className='homepage'>
-      <img src="/orbital.png" className='orbital' />
+    <div className="homepage">
+      <img src="/orbital.png" className="orbital" />
       <div className="left">
         <h1>ZET AI</h1>
         <h2>Supercharge your creativity and productivity</h2>
-        <h3>Make your coding more enjoyable and fast with ZET<br />
+        <h3>
+          Make your coding more enjoyable and fast with ZET
+          <br />
           Join our community today!
         </h3>
-        <Link to="/dashboard">Get Started</Link >
+        <Link to="/dashboard">Get Started</Link>
       </div>
       <div className="right">
         <div className="imgContainer">
           <div className="bgContainer">
             <div className="bg"></div>
           </div>
-          <img src="/bot.png" alt="" className='bot' />
+          <img src="/bot1.webp" alt="" className="bot" />
           <div className="chat">
-            <img src="/bot.png" alt="" />
+            <img
+              src={
+                typingStatus === "Human1"
+                  ? "/human1.avif"
+                  : typingStatus === "Human2"
+                  ? "/human2.avif"
+                  : "/bot.webp"
+              }
+              alt=""
+            />
             <TypeAnimation
               sequence={[
                 // Same substring at the start will only be typed out once, initially
-                'I can write code for you',
-                1000, // wait 1s before replacing "Mice" with "Hamsters"
-                'It is great to help you',
-                1000,
-                'Do you have any innovative ideas?',
-                1000,
-                'Just let me know',
-                1000
+                "How can I help you?",
+                2000,
+                () => {
+                  setTypingStatus("AI-agent");
+                },
+                "Tell me about AI.",
+                2000,
+                () => {
+                  setTypingStatus("Human1");
+                },
+                "AI is a powerful tool to carried out innovative ideas",
+                2000,
+                () => {
+                  setTypingStatus("AI-agent");
+                },
+                "Wow, that`s amazing to use it",
+                2000,
+                () => {
+                  setTypingStatus("Human2");
+                },
               ]}
               wrapper="span"
-              speed={50}
-              style={{ fontSize: '2em', display: 'inline-block' }}
               repeat={Infinity}
+              cursor={true}
+              style={{ fontSize: "1.2em" }}
+              omitDeletionAnimation={true}
             />
           </div>
         </div>
       </div>
+      <div className="terms">
+        <img src="logo.png" alt="" />
+        <div className="links">
+          <Link to="/">Terms of Service</Link>
+          <span>|</span>
+          <Link to="/">Privacy Policy</Link>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
